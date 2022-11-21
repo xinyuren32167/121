0face86c1e2f1c7f2f9d797a63817ac7dd7ca3a2
Glandus73 <gargaren73@gmail.com>
2022-11-21 18:39:35 +0100

    Update perks_general.cpp

--- src/server/scripts/Spells/perks_general.cpp
+++ src/server/scripts/Spells/perks_general.cpp
@@ -293,6 +293,315 @@ class spell_bulwark : public AuraScript
     }
 };
 
+class spell_pyretic_incantation : public AuraScript
+{
+    PrepareAuraScript(spell_pyretic_incantation);
+
+    Aura* GetPerkAura()
+    {
+        if (GetCaster()->HasAura(100079))
+            return GetCaster()->GetAura(100079);
+
+        if (GetCaster()->HasAura(100080))
+            return GetCaster()->GetAura(100080);
+
+        if (GetCaster()->HasAura(100081))
+            return GetCaster()->GetAura(100081);
+
+        if (GetCaster()->HasAura(100082))
+            return GetCaster()->GetAura(100082);
+
+        if (GetCaster()->HasAura(100083))
+            return GetCaster()->GetAura(100083);
+
+        if (GetCaster()->HasAura(100084))
+            return GetCaster()->GetAura(100084);
+
+        return nullptr;
+    }
+
+    int GetProcSpell()
+    {
+        return GetPerkAura()->GetSpellInfo()->GetEffect(EFFECT_0).BasePoints;
+    }
+
+    void HandleProc(AuraEffect const*  /*aurEff*/, ProcEventInfo& eventInfo)
+    {
+        if (eventInfo.GetHitMask() & PROC_EX_CRITICAL_HIT)
+            GetCaster()->AddAura(GetProcSpell(), GetCaster());
+
+        if (eventInfo.GetHitMask() & PROC_EX_NORMAL_HIT && GetCaster()->HasAura(GetProcSpell()))
+            GetCaster()->RemoveAura(GetProcSpell());
+    }
+
+    void Register() override
+    {
+        OnEffectProc += AuraEffectProcFn(spell_pyretic_incantation::HandleProc, EFFECT_0, SPELL_AURA_DUMMY);
+    }
+};
+
+class spell_gutripper : public AuraScript
+{
+    PrepareAuraScript(spell_gutripper);
+
+    Aura* GetPerkAura()
+    {
+        if (GetCaster()->HasAura(100091))
+            return GetCaster()->GetAura(100091);
+
+        if (GetCaster()->HasAura(100092))
+            return GetCaster()->GetAura(100092);
+
+        if (GetCaster()->HasAura(100093))
+            return GetCaster()->GetAura(100093);
+
+        if (GetCaster()->HasAura(100094))
+            return GetCaster()->GetAura(100094);
+
+        if (GetCaster()->HasAura(100095))
+            return GetCaster()->GetAura(100095);
+
+        if (GetCaster()->HasAura(100096))
+            return GetCaster()->GetAura(100096);
+
+        return nullptr;
+    }
+
+    int GetDamagePct()
+    {
+        return GetPerkAura()->GetSpellInfo()->GetEffect(EFFECT_0).BasePoints + 1;
+    }
+
+    int GetProcSpell()
+    {
+        return GetPerkAura()->GetSpellInfo()->GetEffect(EFFECT_1).TriggerSpell;
+    }
+
+    int GetProcChance(ProcEventInfo& eventInfo)
+    {
+        if (eventInfo.GetProcTarget()->GetHealthPct() < 35)
+            return 60;
+
+        return 20;
+    }
+
+    void HandleProc(AuraEffect const*  /*aurEff*/, ProcEventInfo& eventInfo)
+    {
+        uint32 random = urand(1, 100);
+
+        if (random <= GetProcChance(eventInfo))
+        {
+            int32 amount = int32(CalculatePct(eventInfo.GetDamageInfo()->GetDamage(), GetDamagePct()));
+
+            GetCaster()->CastCustomSpell(GetProcSpell(), SPELLVALUE_BASE_POINT0, amount, eventInfo.GetProcTarget(), true);
+        }
+    }
+
+    void Register() override
+    {
+        OnEffectProc += AuraEffectProcFn(spell_gutripper::HandleProc, EFFECT_0, SPELL_AURA_DUMMY);
+    }
+};
+
+class spell_dying_breath : public AuraScript
+{
+    PrepareAuraScript(spell_dying_breath);
+
+    Aura* GetPerkAura()
+    {
+        if (GetCaster()->HasAura(100098))
+            return GetCaster()->GetAura(100098);
+
+        if (GetCaster()->HasAura(100099))
+            return GetCaster()->GetAura(100099);
+
+        if (GetCaster()->HasAura(100100))
+            return GetCaster()->GetAura(100100);
+
+        if (GetCaster()->HasAura(100101))
+            return GetCaster()->GetAura(100101);
+
+        if (GetCaster()->HasAura(100102))
+            return GetCaster()->GetAura(100102);
+
+        if (GetCaster()->HasAura(100103))
+            return GetCaster()->GetAura(100103);
+
+        return nullptr;
+    }
+
+    int GetDamagePct()
+    {
+        return GetPerkAura()->GetSpellInfo()->GetEffect(EFFECT_0).BasePoints + 1;
+    }
+
+    int GetProcSpell()
+    {
+        return GetPerkAura()->GetSpellInfo()->GetEffect(EFFECT_1).TriggerSpell;
+    }
+
+    void HandleProc(AuraEffect const*  /*aurEff*/, ProcEventInfo& eventInfo)
+    {
+        if (!GetCaster()->HasAura(100105))
+        {
+            int32 amount = int32(CalculatePct(GetTarget()->GetMaxHealth(), GetDamagePct()));
+
+            GetCaster()->CastCustomSpell(GetProcSpell(), SPELLVALUE_BASE_POINT0, amount, GetCaster(), TRIGGERED_FULL_MASK);
+            GetCaster()->AddAura(100105, GetCaster());
+        }
+    }
+
+    void Register() override
+    {
+        OnEffectProc += AuraEffectProcFn(spell_dying_breath::HandleProc, EFFECT_0, SPELL_AURA_DUMMY);
+    }
+};
+
+class spell_ysera_s_gift : public AuraScript
+{
+    PrepareAuraScript(spell_ysera_s_gift);
+
+    Aura* GetPerkAura()
+    {
+        if (GetCaster()->HasAura(100106))
+            return GetCaster()->GetAura(100106);
+
+        if (GetCaster()->HasAura(100107))
+            return GetCaster()->GetAura(100107);
+
+        if (GetCaster()->HasAura(100108))
+            return GetCaster()->GetAura(100108);
+
+        if (GetCaster()->HasAura(100109))
+            return GetCaster()->GetAura(100109);
+
+        if (GetCaster()->HasAura(100110))
+            return GetCaster()->GetAura(100110);
+
+        if (GetCaster()->HasAura(100111))
+            return GetCaster()->GetAura(100111);
+
+        return nullptr;
+    }
+
+    int GetProcPct()
+    {
+        return GetPerkAura()->GetSpellInfo()->GetEffect(EFFECT_0).BasePoints + 1;
+    }
+
+    int GetSelfHealSpell()
+    {
+        return GetPerkAura()->GetSpellInfo()->GetEffect(EFFECT_1).TriggerSpell;
+    }
+
+    int GetAllyHealSpell()
+    {
+        return GetPerkAura()->GetSpellInfo()->GetEffect(EFFECT_2).TriggerSpell;
+    }
+
+    void HandlePeriodic(AuraEffect const* aurEff)
+    {
+        int32 amount = int32(CalculatePct(GetCaster()->GetMaxHealth(), GetProcPct()));
+
+        if (GetCaster()->GetHealth() < GetCaster()->GetMaxHealth())
+            GetCaster()->CastCustomSpell(GetSelfHealSpell(), SPELLVALUE_BASE_POINT0, amount, GetCaster(), true);
+        else
+            GetCaster()->CastCustomSpell(GetAllyHealSpell(), SPELLVALUE_BASE_POINT0, amount, GetCaster(), true);
+    }
+
+    void Register() override
+    {
+        OnEffectPeriodic += AuraEffectPeriodicFn(spell_ysera_s_gift::HandlePeriodic, EFFECT_0, SPELL_AURA_PERIODIC_DUMMY);
+    }
+};
+
+class spell_ysera_s_gift_target : public SpellScript
+{
+    PrepareSpellScript(spell_ysera_s_gift_target);
+
+    void FilterTargets(std::list<WorldObject*>& targets)
+    {
+        uint32 const maxTargets = 1;
+
+        if (targets.size() > maxTargets)
+        {
+            targets.sort(Acore::HealthPctOrderPred());
+            targets.resize(maxTargets);
+        }
+    }
+
+    void Register() override
+    {
+        OnObjectAreaTargetSelect += SpellObjectAreaTargetSelectFn(spell_ysera_s_gift_target::FilterTargets, EFFECT_0, TARGET_UNIT_DEST_AREA_ALLY);
+    }
+};
+
+class spell_echo_of_light : public AuraScript
+{
+    PrepareAuraScript(spell_echo_of_light);
+
+    Aura* GetPerkAura()
+    {
+        if (GetCaster()->HasAura(100114))
+            return GetCaster()->GetAura(100114);
+
+        if (GetCaster()->HasAura(100115))
+            return GetCaster()->GetAura(100115);
+
+        if (GetCaster()->HasAura(100116))
+            return GetCaster()->GetAura(100116);
+
+        if (GetCaster()->HasAura(100117))
+            return GetCaster()->GetAura(100117);
+
+        if (GetCaster()->HasAura(100118))
+            return GetCaster()->GetAura(100118);
+
+        if (GetCaster()->HasAura(100119))
+            return GetCaster()->GetAura(100119);
+
+        return nullptr;
+    }
+
+    int GetHealPct()
+    {
+        return GetPerkAura()->GetSpellInfo()->GetEffect(EFFECT_0).BasePoints + 1;
+    }
+
+    int GetProcSpell()
+    {
+        return GetPerkAura()->GetSpellInfo()->GetEffect(EFFECT_1).TriggerSpell;
+    }
+
+    void HandleProc(AuraEffect const*  /*aurEff*/, ProcEventInfo& eventInfo)
+    {
+        int32 totalTicks = sSpellMgr->AssertSpellInfo(GetProcSpell())->GetMaxTicks();
+        int32 amount = int32(CalculatePct(eventInfo.GetHealInfo()->GetHeal(), GetHealPct()) / totalTicks);
+
+        if (eventInfo.GetProcTarget()->HasAura(GetProcSpell()))
+        {
+            Aura* allyHealAura = eventInfo.GetProcTarget()->GetAura(GetProcSpell());
+
+            int32 healAmountPerTick = allyHealAura->GetEffect(EFFECT_0)->GetAmount() / totalTicks;
+            int32 remainingTicks = allyHealAura->GetDuration() / allyHealAura->GetEffect(EFFECT_0)->GetAmplitude();
+            int32 remainingAmount = healAmountPerTick * remainingTicks;
+            int32 remainingAmountPerTick = remainingAmount / totalTicks;
+
+            amount += remainingAmountPerTick;
+
+            if (amount > (GetCaster()->GetMaxHealth() * 0.35))
+                amount = (GetCaster()->GetMaxHealth() * 0.35);
+        }
+
+        eventInfo.GetProcTarget()->CastDelayedSpellWithPeriodicAmount(eventInfo.GetActor(), GetProcSpell(), SPELL_AURA_PERIODIC_HEAL, amount);
+    }
+
+    void Register() override
+    {
+        OnEffectProc += AuraEffectProcFn(spell_echo_of_light::HandleProc, EFFECT_0, SPELL_AURA_DUMMY);
+    }
+};
+
 void AddSC_generals_perks_scripts()
 {
     RegisterSpellScript(spell_vampirism);
@@ -301,4 +610,10 @@ void AddSC_generals_perks_scripts()
     RegisterSpellScript(spell_overwhelming_power);
     RegisterSpellScript(spell_overwhelming_power_stack_remove);
     RegisterSpellScript(spell_bulwark);
+    RegisterSpellScript(spell_pyretic_incantation);
+    RegisterSpellScript(spell_gutripper);
+    RegisterSpellScript(spell_dying_breath);
+    RegisterSpellScript(spell_ysera_s_gift);
+    RegisterSpellScript(spell_ysera_s_gift_target);
+    RegisterSpellScript(spell_echo_of_light);
 }
