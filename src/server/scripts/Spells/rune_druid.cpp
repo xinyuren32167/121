e6c0e4c5c3bbfd39aa8c2eaee4f7765eafd3bc2e
Glandus73 <gargaren73@gmail.com>
2023-04-19 18:21:04 +0200

    Update rune_druid.cpp

--- src/server/scripts/Spells/rune_druid.cpp
+++ src/server/scripts/Spells/rune_druid.cpp
@@ -11,16 +11,41 @@
 
 enum DruidSpells
 {
-    SPELL_DRUID_NAME = 000,
-    
+    //forms
+    FORM_AQUATIC_FORM = 1066,
+    FORM_BEAR_FORM = 5487,
+    FORM_DIRE_BEAR_FORM = 9634,
+    FORM_URSOC_FORM = 000,
+    FORM_CAT_FORM = 768,
+    FORM_ASHAMANE_FORM = 000,
+    FORM_FLIGHT_FORM = 33943,
+    FORM_SWIFT_FLIGHT_FORM = 40120,
+    FORM_TRAVEL_FORM = 783,
+    FORM_SWIFT_TRAVEL_FORM = 000,
+    FORM_MOONKIN_FORM = 24858,
+    FORM_TREE_FORM = 000,
 
-    RUNE_DRUID_NAME_TYPE = 000,
+
+
+    SPELL_BARKSKIN = 22812,
+    SPELL_INNERVATE = 29166,
+    SPELL_STARFALL = 53251,
+    SPELL_TIGERS_FURY = 50213,
+    SPELL_WILD_GROWTH = 53251,
+
+
+    RUNE_DRUID_PROTECTIVE_SKIN_SHIELD = 700050,
 
 };
 
 class rune_druid_lycara_fleeting_glimpse : public AuraScript
 {
     PrepareAuraScript(rune_druid_lycara_fleeting_glimpse);
+
+    bool CheckProc(ProcEventInfo& eventInfo)
+    {
+        return GetCaster()->IsInCombat();
+    }
 
     void HandleProc(AuraEffect const* aurEff)
     {
@@ -29,12 +54,159 @@ class rune_druid_lycara_fleeting_glimpse : public AuraScript
         if (!caster || caster->isDead())
             return;
 
-        caster->SetPower(POWER_ENERGY, caster->GetPower(POWER_ENERGY) + 1);
+        if (GetCaster()->HasAura(FORM_AQUATIC_FORM) || GetCaster()->HasAura(FORM_FLIGHT_FORM) || GetCaster()->HasAura(FORM_SWIFT_FLIGHT_FORM) || GetCaster()->HasAura(FORM_TRAVEL_FORM) || GetCaster()->HasAura(FORM_SWIFT_TRAVEL_FORM))
+            return;
+
+        if (GetCaster()->HasAura(FORM_BEAR_FORM) || GetCaster()->HasAura(FORM_DIRE_BEAR_FORM) || GetCaster()->HasAura(FORM_URSOC_FORM))
+        {
+            int32 coolDown = caster->GetSpellCooldownDelay(SPELL_BARKSKIN);
+            int32 arcaneShotCooldown = sSpellMgr->GetSpellInfo(SPELL_BARKSKIN)->GetRecoveryTime();
+            int32 cooldownReduction = arcaneShotCooldown - coolDown;
+
+            caster->CastSpell(GetCaster(), SPELL_BARKSKIN, TRIGGERED_FULL_MASK);
+            caster->ModifySpellCooldown(SPELL_BARKSKIN, -cooldownReduction);
+            return;
+        }
+
+        if (GetCaster()->HasAura(FORM_CAT_FORM) || GetCaster()->HasAura(FORM_ASHAMANE_FORM))
+        {
+            int32 coolDown = caster->GetSpellCooldownDelay(SPELL_TIGERS_FURY);
+            int32 arcaneShotCooldown = sSpellMgr->GetSpellInfo(SPELL_TIGERS_FURY)->GetRecoveryTime();
+            int32 cooldownReduction = arcaneShotCooldown - coolDown;
+
+            caster->CastSpell(GetCaster(), SPELL_TIGERS_FURY, TRIGGERED_FULL_MASK);
+            caster->ModifySpellCooldown(SPELL_TIGERS_FURY, -cooldownReduction);
+            return;
+        }
+
+        if (GetCaster()->HasAura(FORM_MOONKIN_FORM))
+        {
+            caster->CastSpell(GetCaster(), SPELL_STARFALL, TRIGGERED_FULL_MASK);
+
+            return;
+        }
+
+
+        int32 coolDown = caster->GetSpellCooldownDelay(SPELL_WILD_GROWTH);
+        int32 arcaneShotCooldown = sSpellMgr->GetSpellInfo(SPELL_WILD_GROWTH)->GetRecoveryTime();
+        int32 cooldownReduction = arcaneShotCooldown - coolDown;
+
+        caster->CastSpell(GetCaster(), SPELL_WILD_GROWTH, TRIGGERED_FULL_MASK);
+        caster->ModifySpellCooldown(SPELL_WILD_GROWTH, -cooldownReduction);
     }
 
     void Register()
     {
+        DoCheckProc += AuraCheckProcFn(rune_druid_lycara_fleeting_glimpse::CheckProc);
         OnEffectPeriodic += AuraEffectPeriodicFn(rune_druid_lycara_fleeting_glimpse::HandleProc, EFFECT_0, SPELL_AURA_PERIODIC_DUMMY);
+    }
+};
+
+class rune_druid_lycara_teachings : public AuraScript
+{
+    PrepareAuraScript(rune_druid_lycara_teachings);
+
+    Aura* GetRuneAura()
+    {
+        for (size_t i = 700026; i < 700032; i++)
+        {
+            if (GetCaster()->HasAura(i))
+                return GetCaster()->GetAura(i);
+        }
+
+        return nullptr;
+    }
+
+    void HandleProc(AuraEffect const* aurEff, AuraEffectHandleModes mode)
+    {
+        if (!GetRuneAura() || GetCaster()->isDead())
+            return;
+
+        int32 buffAura = GetRuneAura()->GetSpellInfo()->GetEffect(EFFECT_2).TriggerSpell;
+
+        GetCaster()->AddAura(buffAura, GetCaster());
+    }
+
+    void HandleRemove(AuraEffect const* aurEff, AuraEffectHandleModes mode)
+    {
+        for (size_t i = 700032; i < 700038; i++)
+        {
+            if (GetCaster()->HasAura(i))
+                 GetCaster()->RemoveAura(i);
+        }
+    }
+
+    void Register() override
+    {
+        OnEffectApply += AuraEffectApplyFn(rune_druid_lycara_teachings::HandleProc, EFFECT_0, SPELL_AURA_MOD_SHAPESHIFT, AURA_EFFECT_HANDLE_REAL);
+        OnEffectRemove += AuraEffectRemoveFn(rune_druid_lycara_teachings::HandleRemove, EFFECT_0, SPELL_AURA_MOD_SHAPESHIFT, AURA_EFFECT_HANDLE_REAL);
+    }
+};
+
+class rune_druid_natural_wisdom : public AuraScript
+{
+    PrepareAuraScript(rune_druid_natural_wisdom);
+
+    Aura* GetRuneAura(Unit* caster)
+    {
+        for (size_t i = 700038; i < 700044; i++)
+        {
+            if (caster->HasAura(i))
+                return caster->GetAura(i);
+        }
+
+        return nullptr;
+    }
+
+    void HandleProc(AuraEffect const* aurEff, AuraEffectHandleModes mode)
+    {
+        Unit* caster = GetCaster();
+
+        if (!GetRuneAura(caster) || caster->isDead())
+            return;
+
+        if (GetUnitOwner() == caster)
+            return;
+
+        caster->AddAura(SPELL_INNERVATE, caster);
+    }
+
+    void HandleRemove(AuraEffect const* aurEff, AuraEffectHandleModes mode)
+    {
+        for (size_t i = 700032; i < 700038; i++)
+        {
+            if (GetCaster()->HasAura(i))
+                GetCaster()->RemoveAura(i);
+        }
+    }
+
+    void Register() override
+    {
+        OnEffectApply += AuraEffectApplyFn(rune_druid_natural_wisdom::HandleProc, EFFECT_0, SPELL_AURA_PERIODIC_ENERGIZE, AURA_EFFECT_HANDLE_REAL);
+        OnEffectRemove += AuraEffectRemoveFn(rune_druid_natural_wisdom::HandleRemove, EFFECT_0, SPELL_AURA_PERIODIC_ENERGIZE, AURA_EFFECT_HANDLE_REAL);
+    }
+};
+
+class rune_druid_protective_skin : public AuraScript
+{
+    PrepareAuraScript(rune_druid_protective_skin);
+
+    void HandleProc(AuraEffect const* aurEff, ProcEventInfo& eventInfo)
+    {
+        Unit* caster = GetCaster();
+
+        if (caster->isDead())
+            return;
+
+        int32 maxHealth = caster->GetMaxHealth();
+        int32 amount = int32(CalculatePct(maxHealth, aurEff->GetAmount()));
+
+        GetCaster()->CastCustomSpell(RUNE_DRUID_PROTECTIVE_SKIN_SHIELD, SPELLVALUE_BASE_POINT0, amount, caster, TRIGGERED_FULL_MASK);
+    }
+
+    void Register()
+    {
+        OnEffectProc += AuraEffectProcFn(rune_druid_protective_skin::HandleProc, EFFECT_0, SPELL_AURA_DUMMY);
     }
 };
 
@@ -43,7 +215,11 @@ class rune_druid_lycara_fleeting_glimpse : public AuraScript
 void AddSC_druid_rune_scripts()
 {
     RegisterSpellScript(rune_druid_lycara_fleeting_glimpse);
+    RegisterSpellScript(rune_druid_lycara_teachings);
+    RegisterSpellScript(rune_druid_natural_wisdom);
+    RegisterSpellScript(rune_druid_protective_skin);
+
+
     
-
-
+        
 }
