8389dededc8dd753fa3b3d8428f6928355e2b6c4
Matth <matthieu.defrenne@gmail.com>
2024-01-03 16:38:37 +0100

    Exception

--- modules/mod-learn-spell/src/LearningSpellsManager.cpp
+++ modules/mod-learn-spell/src/LearningSpellsManager.cpp
@@ -10,8 +10,29 @@ enum SpellWarrior {
 
 
 enum SpellHunter {
-
+    SPELL_HUNTER_BEAST_LORE = 1462,
+    SPELL_HUNTER_CALL_PET = 883,
+    SPELL_HUNTER_CALL_STABLED_PET = 62757,
+    SPELL_HUNTER_DISMISS_PET = 2641,
+    SPELL_HUNTER_FEED_PET = 6991,
+    SPELL_HUNTER_MEND_PET = 48990,
+    SPELL_HUNTER_REVIVE_PET = 982,
+    SPELL_HUNTER_TAME_PET = 1515,
+    SPELL_HUNTER_CAMOUFLAGE = 80163,
+    SPELL_HUNTER_ARCANE_SHOT = 49045,
+    SPELL_HUNTER_MASTERY_FROM_THE_SHADOW = 499996,
 };
+
+enum WarlockSpells
+{
+    SPELL_WARLOCK_SUMMON_FELGUARD = 30146,
+    SPELL_WARLOCK_SUMMON_FELHUNTER = 691,
+    SPELL_WARLOCK_SUMMON_IMP = 688,
+    SPELL_WARLOCK_SUMMON_SUCCUBUS = 712,
+    SPELL_WARLOCK_SUMMON_VOIDWALKER = 697,
+    SPELL_WARLOCK_SUMMON_EYE_OF_KILLROG = 126,
+};
+
 
 void LearningSpellsManager::PreloadAllSpells()
 {
@@ -38,7 +59,26 @@ bool LearningSpellsManager::Exception(Player* player, uint32 spellId)
     if (spellId == SPELL_WARRIOR_VICTORY_RUSH && player->HasSpell(SPELL_WARRIOR_IMPEDING_VICTORY))
         return false;
 
+    if ((spellId == SPELL_WARLOCK_SUMMON_FELGUARD
+        || spellId == SPELL_HUNTER_CALL_PET
+        || spellId == SPELL_HUNTER_CALL_STABLED_PET
+        || spellId == SPELL_HUNTER_DISMISS_PET
+        || spellId == SPELL_HUNTER_FEED_PET
+        || spellId == SPELL_HUNTER_MEND_PET
+        || spellId == SPELL_HUNTER_REVIVE_PET
+        || spellId == SPELL_HUNTER_TAME_PET
+        || spellId == SPELL_HUNTER_CAMOUFLAGE
+        || spellId == SPELL_HUNTER_ARCANE_SHOT) && player->HasSpell(SPELL_HUNTER_MASTERY_FROM_THE_SHADOW))
+            return false;
 
+
+        if ((spellId == SPELL_HUNTER_BEAST_LORE
+            || spellId == SPELL_WARLOCK_SUMMON_FELHUNTER
+            || spellId == SPELL_WARLOCK_SUMMON_IMP
+            || spellId == SPELL_WARLOCK_SUMMON_SUCCUBUS
+            || spellId == SPELL_WARLOCK_SUMMON_VOIDWALKER
+            || spellId == SPELL_WARLOCK_SUMMON_EYE_OF_KILLROG) && player->HasSpell(SPELL_HUNTER_MASTERY_FROM_THE_SHADOW))
+            return false;
 
 
     return true;
