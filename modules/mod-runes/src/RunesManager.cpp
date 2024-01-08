4c67d8438275ae13fff36351f7e2238ab444668f
Matth <matthieu.defrenne@gmail.com>
2024-01-08 11:35:54 +0100

    Update RunesManager.cpp

--- modules/mod-runes/src/RunesManager.cpp
+++ modules/mod-runes/src/RunesManager.cpp
@@ -21,7 +21,7 @@ RuneConfig RunesManager::config = {};
 void RunesManager::SetupConfig()
 {
     config.enabled = sConfigMgr->GetOption<bool>("RuneManager.enabled", true);
-    config.debug = false;
+    config.debug = true;
     config.maxSlots = 10;
     config.defaultSlot = 10;
 
